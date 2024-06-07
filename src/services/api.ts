d7837b71f3701686fd5280da1ca2fe35a2ecb092
leonarjin <izumi.jin@live.com>
2024-06-07 06:58:23 +0100

    hotfixes

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,10 @@
 import axios from 'axios'
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333'
-})
+    baseURL: process.env.REACT_APP_BACKEND
+});
+ 
 export default api;
